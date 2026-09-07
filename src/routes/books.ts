e7b84b5d424106c18da6792782b027b8fc76302d
Brayden Pankaskie <bpankask@Braydens-MacBook-Pro-2.local>
2026-09-07 12:52:39 -0500

    forgot to commit change to route

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 // define the home page route
 router.get('/', async (req: Request, res: Response) => {
   const books = await getAllBooks();
-
+  
   res.send(books);
 });
 
